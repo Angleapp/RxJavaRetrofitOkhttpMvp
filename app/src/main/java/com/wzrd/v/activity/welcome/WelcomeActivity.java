ee940eeec81d6457685453075e0e4bdd84332f49
lkxiaojian <18513075404@163.com>
2017-12-31 10:19:50 +0800

    实现rxJava2短信倒计时

--- app/src/main/java/com/wzrd/v/activity/welcome/WelcomeActivity.java
+++ app/src/main/java/com/wzrd/v/activity/welcome/WelcomeActivity.java
@@ -2,14 +2,95 @@ package com.wzrd.v.activity.welcome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.wzrd.R;
+import com.wzrd.m.utils.Utils;
+import com.wzrd.p.RxTimerPresenter;
+import com.wzrd.v.view.interf.TimerView;
 
-public class WelcomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WelcomeActivity extends AppCompatActivity implements TimerView {
+
+    @BindView(R.id.et_phonenum)
+    EditText etPhonenum;
+    @BindView(R.id.tv_security)
+    TextView tvSecurity;
+    @BindView(R.id.et_code)
+    EditText etCode;
+    @BindView(R.id.tv_sendsecurity)
+    TextView tvSendsecurity;
+    @BindView(R.id.bt_send)
+    Button btSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.et_phonenum, R.id.tv_security, R.id.et_code, R.id.tv_sendsecurity, R.id.bt_send})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.et_phonenum:
+                break;
+            case R.id.tv_security:
+                break;
+            case R.id.et_code:
+                break;
+            case R.id.tv_sendsecurity:
+                RxTimerPresenter presenter=new RxTimerPresenter(WelcomeActivity.this);
+                presenter.timer(10);
+                break;
+            case R.id.bt_send:
+//                send();
+
+
+
+                break;
+        }
+    }
+
+    /**
+     * 点击开始之后的逻辑
+     */
+
+    private void send() {
+        boolean mobileNO = Utils.isMobileNO(etPhonenum.getText().toString());
+        if(mobileNO){
+
+        }else{
+            Utils.ToastShort(WelcomeActivity.this,"请输入正确的手机号");
+        }
+    }
+
+    @Override
+    public void onCompile() {
+        tvSendsecurity.setText("点击发送验证码");
+        tvSendsecurity.setEnabled(true);
+    }
+
+    @Override
+    public void onRefresh(Object message) {
+        tvSendsecurity.setText(message+"可以重新发送");
+    }
+
+    @Override
+    public void onError(Object message) {
+        tvSendsecurity.setText("点击发送验证码");
+        tvSendsecurity.setEnabled(true);
+    }
+
+    @Override
+    public void onBegin(Object message) {
+        tvSendsecurity.setEnabled(false);
+        tvSendsecurity.setText(message+"可以重新发送");
     }
 }
