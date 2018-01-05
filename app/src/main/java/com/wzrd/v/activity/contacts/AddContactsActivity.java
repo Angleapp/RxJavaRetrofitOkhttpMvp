b39a19078778e53b6fd284780d6afc9547ae7924
lkxiaojian <18513075404@163.com>
2018-01-05 16:19:20 +0800

    联系人

---
+++ app/src/main/java/com/wzrd/v/activity/contacts/AddContactsActivity.java
@@ -0,0 +1,161 @@
+package com.wzrd.v.activity.contacts;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+import com.wzrd.R;
+import com.wzrd.m.been.TSYSCONTANTS;
+import com.wzrd.m.been.TSYSUSER;
+import com.wzrd.m.contacts.ContactsActivity;
+import com.wzrd.m.db.manger.ContactsManager;
+import com.wzrd.m.db.manger.UserManager;
+import com.wzrd.m.utils.Constants;
+import com.wzrd.m.utils.DateUtils;
+import com.wzrd.m.utils.SharedPreferencesUtil;
+import com.wzrd.m.utils.Utils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+public class AddContactsActivity extends AppCompatActivity {
+
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.et_contactphonenum)
+    EditText etContactphonenum;
+    @BindView(R.id.iv_contants)
+    ImageView ivContants;
+    @BindView(R.id.bt_add)
+    Button btAdd;
+    private final int CONTSCTRESULT = 0x09;
+    private String name;
+    private String lovenum;
+    private String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contacts);
+        message = getIntent().getExtras().getString("message");
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.iv_back, R.id.iv_contants, R.id.bt_add})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.iv_contants:
+                start();
+                break;
+            case R.id.bt_add:
+                String username = SharedPreferencesUtil.getString(this, "nickname", "");
+                String userphonenum = SharedPreferencesUtil.getString(this, "userphonenum", "");
+                String icon = SharedPreferencesUtil.getString(this, "icon", "");
+
+                if ("love".equals(message)) {
+                    UserManager manager = new UserManager(this);
+                    TSYSUSER tsysuser = new TSYSUSER();
+                    tsysuser.setT_sys_loverphone(lovenum);
+                    tsysuser.setT_sys_lover_name(name);
+                    tsysuser.setT_sys_userid(userphonenum);
+                    tsysuser.setT_sys_usericonpath(icon);
+                    tsysuser.setT_sys_userphone(userphonenum);
+                    tsysuser.setT_sys_modify_time(DateUtils.getCurrentDate());
+                    tsysuser.setT_sys_id(userphonenum);
+                    tsysuser.setT_sys_username(username);
+                    tsysuser.setT_sys_modify_id(userphonenum);
+                    manager.insertUser(tsysuser);
+
+
+
+                } else {
+                    if (lovenum != null) {
+
+                        ContactsManager manager = new ContactsManager(this);
+                        TSYSCONTANTS tsyscontants = new TSYSCONTANTS();
+                        tsyscontants.setT_sys_contacts_id(lovenum);
+                        tsyscontants.setT_sys_contacts_name(name);
+                        tsyscontants.setT_sys_contactsconpath(icon);
+                        tsyscontants.setT_sys_id(lovenum);
+                        tsyscontants.setT_sys_modify_time(DateUtils.getCurrentDate());
+                        tsyscontants.setT_sys_userid(userphonenum);
+                        tsyscontants.setT_sys_userphone(userphonenum);
+                        tsyscontants.setT_sys_usericonpath(icon);
+                        manager.insertUser(tsyscontants);
+                    }
+                }
+                finish();
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CONTSCTRESULT && data != null) {
+            name = (String) data.getExtras().get("lovename");
+            lovenum = (String) data.getExtras().get("lovenum");
+            etContactphonenum.setText(name);
+        }
+    }
+
+    public void start() {
+        RxPermissions rxPermissions = new RxPermissions(AddContactsActivity.this);
+        rxPermissions
+                .request(Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SYNC_SETTINGS)
+                .observeOn(AndroidSchedulers.mainThread())
+
+                .subscribe(new Observer<Boolean>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        /**
+                         * aBoolean为true  同意权限
+                         * aBoolean为false 没同意权限
+                         */
+
+                        if (!aBoolean) {
+                            Utils.ToastShort(AddContactsActivity.this, Constants.RDWISDCONTANTS);
+                        } else {
+                            Intent intent = new Intent(AddContactsActivity.this, ContactsActivity.class);
+                            intent.putExtra("pathfrom", "add");
+                            AddContactsActivity.this.startActivityForResult(intent, CONTSCTRESULT);
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+    }
+}
---
+++ app/src/main/java/com/wzrd/v/adapter/ReclycleContactAdapter.java
@@ -0,0 +1,90 @@
+package com.wzrd.v.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.wzrd.R;
+import com.wzrd.m.been.TSYSCONTANTS;
+import com.wzrd.m.db.manger.ContactsManager;
+import com.wzrd.v.view.GlideCircleTransform;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lk on 2018/1/5.
+ */
+
+public class ReclycleContactAdapter extends RecyclerView.Adapter<ReclycleContactAdapter.ViewHolder> {
+    private Context mContext;
+    private List<TSYSCONTANTS> tsyscontantsList;
+
+    public ReclycleContactAdapter(Context mContext, List<TSYSCONTANTS> tsyscontantsList) {
+        this.mContext = mContext;
+        this.tsyscontantsList = tsyscontantsList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.contact_items, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        View view = holder.itemView;
+        final TSYSCONTANTS modlie = tsyscontantsList.get(position);
+        holder.tv_contact_name.setText(modlie.getT_sys_contacts_name());
+
+        Glide.with(view.getContext())
+                .load(modlie.getT_sys_usericonpath())
+                .placeholder(R.mipmap.feilei_on)
+                .bitmapTransform(new GlideCircleTransform(view.getContext()))
+                .error(R.mipmap.feilei_on)
+                .into(holder.iv_contact_iocn);
+        holder.btn_delcontact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Utils.ToastShort(mContext, "删除");
+                ContactsManager manager = new ContactsManager(mContext);
+                manager.deleteUser(modlie);
+                notifyDataSetChanged();
+                List<TSYSCONTANTS> list = new ArrayList<>();
+                tsyscontantsList.remove(modlie);
+                list.addAll(tsyscontantsList);
+                tsyscontantsList.clear();
+                tsyscontantsList.addAll(list);
+                notifyDataSetChanged();
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tsyscontantsList == null ? 0 : tsyscontantsList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_contact_name;
+        ImageView iv_contact_iocn;
+        Button btn_delcontact;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv_contact_name = (TextView) itemView.findViewById(R.id.tv_contact_name);
+            iv_contact_iocn = (ImageView) itemView.findViewById(R.id.iv_contact_iocn);
+            btn_delcontact = (Button) itemView.findViewById(R.id.btn_delcontact);
+        }
+    }
+}
