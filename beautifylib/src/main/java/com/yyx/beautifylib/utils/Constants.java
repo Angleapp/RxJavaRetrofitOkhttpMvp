a10e8f48173cd9c7f6a8eea645483933dbbd72ce
lkxiaojian <18513075404@163.com>
2018-01-11 15:45:31 +0800

    语音

---
+++ beautifylib/src/main/java/com/yyx/beautifylib/utils/Constants.java
@@ -0,0 +1,21 @@
+package com.yyx.beautifylib.utils;
+
+/**
+ * Created by lk on 2017/10/30.
+ */
+
+public class Constants {
+
+    /**
+     * 关于数据库
+     */
+    //数据库文件存储的路径
+    public static String DATABASE_PATH = "/wzrd/database";
+    public static String VIDEOS_PATH="/wzrd/videos";
+
+    public static boolean isshow=true;
+
+    public static final String RDWISDPREMISS="您已经把读取sd卡权限拒绝了，请重新开启后再次使用";
+
+    public static final String RDWISDCONTANTS="您已经把读取联系人的权限拒绝了，请重新开启后再次使用";
+}
