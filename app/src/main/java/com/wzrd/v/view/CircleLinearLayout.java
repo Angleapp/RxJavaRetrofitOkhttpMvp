baf963fd6c135681973056948876be65985bad5e
lkxiaojian <18513075404@163.com>
2018-02-14 07:52:53 +0800

    主页面的布局

--- app/src/main/java/com/wzrd/v/view/CircleLinearLayout.java
+++ app/src/main/java/com/wzrd/v/view/CircleLinearLayout.java
@@ -37,7 +37,12 @@ public class CircleLinearLayout extends ViewGroup {
             int attNameId = typedArray.getIndex(i);
             switch (attNameId) {
                 case R.styleable.CircleLinearLayout_circleRadius:
-                    radius = typedArray.getDimensionPixelSize(attNameId+1, defvalue);
+                    try {
+                        radius = typedArray.getDimensionPixelSize(attNameId, defvalue);
+                    }catch (Exception e){
+                        Log.e("e",e.getMessage().toString());
+                    }
+
                     break;
             }
         }
