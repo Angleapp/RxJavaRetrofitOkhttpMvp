fb40557669e779b7eed100f301bc23ca442f2c23
lkxiaojian <18513075404@163.com>
2018-02-23 17:08:48 +0800

    收件箱下拉刷新上啦加载更多

--- app/src/main/java/com/wzrd/v/fragment/contanct/InboxFragment.java
+++ app/src/main/java/com/wzrd/v/fragment/contanct/InboxFragment.java
@@ -3,8 +3,8 @@ package com.wzrd.v.fragment.contanct;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +12,12 @@ import android.view.ViewGroup;
 import com.wzrd.R;
 import com.wzrd.databinding.InboxFragmentBinding;
 import com.wzrd.m.been.ContactMessage;
-import com.wzrd.m.been.InboxContactMessage;
 import com.wzrd.m.utils.DateUtils;
 import com.wzrd.m.utils.SharedPreferencesUtil;
-import com.wzrd.v.activity.welcome.IconActivity;
+import com.wzrd.m.utils.Utils;
+import com.wzrd.p.Rypresenter;
 import com.wzrd.v.fragment.base.NoNetBaseLayFragment;
+import com.wzrd.v.view.RecycleResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,19 +26,47 @@ import java.util.List;
  * Created by lk on 2018/2/9.
  */
 
-public class InboxFragment extends NoNetBaseLayFragment{
+public class InboxFragment extends NoNetBaseLayFragment  implements RecycleResult {
     private  InboxFragmentBinding dataBinding;
     private String TAG="InboxFragment";
     private String imagepath;
+    private  SwipeRefreshLayout swlaout;
+    private RecyclerView  recycle;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-         dataBinding= DataBindingUtil.inflate(inflater, R.layout.inbox_fragment, container, false);
+        dataBinding= DataBindingUtil.inflate(inflater, R.layout.inbox_fragment, container, false);
         imagepath= SharedPreferencesUtil.getString(getContext(),"icon","");
         isPrepared = true;
         intidata();
+        Refresh();
+        Rypresenter rypresenter = new Rypresenter(this);
+        rypresenter.getlast(recycle, "inbox");
         return dataBinding.getRoot();
+    }
+
+    /**
+     * 数据刷新
+     */
+    private void Refresh() {
+
+        recycle= (RecyclerView) dataBinding.getRoot().getRootView().findViewById(R.id.recycle);
+        swlaout= (SwipeRefreshLayout) dataBinding.getRoot().getRootView().findViewById(R.id.swlaout);
+        swlaout.setSize(0);
+        swlaout.setProgressBackgroundColorSchemeResource(android.R.color.white);
+        swlaout.setColorSchemeResources(android.R.color.holo_blue_light,
+                android.R.color.holo_red_light,android.R.color.holo_orange_light,
+                android.R.color.holo_green_light);
+        swlaout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Utils.ToastShort(getContext(),"此处可以填写下拉刷新逻辑");
+                swlaout.setRefreshing(false);
+
+            }
+        });
+
     }
 
     /**
@@ -47,7 +76,7 @@ public class InboxFragment extends NoNetBaseLayFragment{
         List<ContactMessage> list = new ArrayList<>();
 
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 19; i++) {
             ContactMessage message = new ContactMessage();
             message.setMotifitytime(DateUtils.getCurrentDate());
             message.setIconpath(imagepath);
@@ -63,5 +92,15 @@ public class InboxFragment extends NoNetBaseLayFragment{
             return;
         }
 //        intidata();
+    }
+
+    /**
+     * 判断滑动到底部
+     * @param message
+     */
+    @Override
+    public void last(String message) {
+        Utils.ToastShort(getContext(),"滑动到底部.此处可以填写上拉刷新逻辑");
+
     }
 }
